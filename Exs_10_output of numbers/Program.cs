a6f2620327de37d44cc17cc00d643d43c5421c0d
Aleksey-pops <122688170+Aleksey-pops@users.noreply.github.com>
2023-01-28 23:35:07 +0300

    output of numbers

---
+++ Exs_10_output of numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число: ");
+int numberA = int.Parse(Console.ReadLine ());
+int numberB = numberA * (-1);
+while(numberB < numberA);
+{
+    Console.WriteLine(numberB);
+    numberB++;
+}
+
+//{
+//   Console.WriteLine("выводим числа: " + numberA);
+//}
+//int numberB = numberA * (-1);
+//while(numberA <= numberB);
+//
+//{
+//    Console.WriteLine(numberB);
+//    numberB++;
+//}
