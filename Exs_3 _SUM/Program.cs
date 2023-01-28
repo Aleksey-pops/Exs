ca564ccb1736a9e8ecb8afa81b3ff3182b267da2
Aleksey-pops <122688170+Aleksey-pops@users.noreply.github.com>
2023-01-28 18:23:51 +0300

    Added SUM

--- Exs_3 _SUM/Program.cs
+++ Exs_3 _SUM/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numberA = 3;
+int numberb = 5;
+int result = numberA + numberb;
+Console.WriteLine(result);
