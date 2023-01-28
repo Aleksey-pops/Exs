f6f5aae372e250533e26f25dfee94c5de17d5fe0
Aleksey-pops <122688170+Aleksey-pops@users.noreply.github.com>
2023-01-28 22:22:55 +0300

    Which number is greater or less

---
+++ Exs_9_BIG_ min/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите 1 число: ");
+int First = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите 2 число: ");
+int Second = int.Parse(Console.ReadLine());
+
+if(First > Second)
+{
+    Console.WriteLine("Максимально число: " + First);
+    Console.WriteLine("Минимальное число: " + Second);
+}
+else
+{
+    Console.WriteLine("Максимально число: " + Second);
+    Console.WriteLine("Минимальное число: " + First);
+}
